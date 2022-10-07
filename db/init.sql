84cda56ebdc7ed4d9ee4fe0946962680cabee4f7
mdalamaladm <muhammaddzakyalam@gmail.com>
2022-10-08 05:42:29 +0700

    sql: remove age

--- db/init.sql
+++ db/init.sql
@@ -6,7 +6,6 @@ CREATE TABLE user_data (
 CREATE TABLE profile (
   username VARCHAR(100) PRIMARY KEY,
   name VARCHAR(100),
-  age INT,
   nationality VARCHAR(50),
   favorite_weather VARCHAR(50),
   FOREIGN KEY (username) REFERENCES user_data(username)
