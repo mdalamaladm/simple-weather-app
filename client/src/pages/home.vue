de4ba481ebf734b4473560e2ed0eed38e5b39105
mdalamaladm <muhammaddzakyalam@gmail.com>
2022-10-08 05:48:51 +0700

    home: add link to profile + add openweather attribution + fix getCurrentPosition

--- client/src/pages/home.vue
+++ client/src/pages/home.vue
@@ -2,6 +2,9 @@
   <div>
     <h2>Welcome, {{ name }} to...</h2>
     <h1>Simple Weather App</h1>
+    <NuxtLink to="/profile">
+      Tell us more about you
+    </NuxtLink>
     <div v-if="blocked">
       <p>
         Unfortunately, We cannot find your current location
@@ -15,6 +18,8 @@
       <p>This is your current weather in your location</p>
       {{ weather }}
     </div>
+    <img src="/open-weather.png" alt="">
+    <p>Weather data provided by <a href="https://openweathermap.org/">OpenWeather</a></p>
   </div>
 </template>
 
@@ -30,23 +35,34 @@ export default {
   },
   async mounted () {
     const profile = await this.$api.$get('/profile')
-    console.log(profile)
     this.name = profile.name
 
-    navigator.geolocation.getCurrentPosition(async (position) => {
-      const lat = position.coords.latitude
-      const lon = position.coords.longitude
+    navigator.geolocation.getCurrentPosition(
+      async (position) => {
+        const lat = position.coords.latitude
+        const lon = position.coords.longitude
 
-      console.log(lat, lon)
+        console.log(lat, lon)
 
-      if (lat && lon) {
-        this.weather = await this.$api.$get('/weather', {
-          params: {
-            lat,
-            lon
-          }
-        })
-      } else {
+        if (lat && lon) {
+          this.weather = await this.$api.$get('/weather', {
+            params: {
+              lat,
+              lon
+            }
+          })
+        } else {
+          this.blocked = true
+
+          this.weather = await this.$api.$get('/weather', {
+            params: {
+              lat: 6.2088,
+              lon: 106.8456
+            }
+          })
+        }
+      },
+      async () => {
         this.blocked = true
 
         this.weather = await this.$api.$get('/weather', {
@@ -56,7 +72,7 @@ export default {
           }
         })
       }
-    })
+    )
   }
 }
 </script>
